6a6ebdc4de1a3e15c7840e463ecfb9eb1efbc287
Vladimir Vozar <vladimirvozar@gmail.com>
2020-04-18 21:54:06 +0200

    Setting up controllers, configuren Json setting to fix reference loop handling

---
+++ ComedyEvents/Controllers/EventsController.cs
@@ -0,0 +1,39 @@
+﻿using ComedyEvents.Models;
+using ComedyEvents.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComedyEvents.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        private readonly IEventRepository _eventRepository;
+
+        public EventsController(IEventRepository eventRepository)
+        {
+            _eventRepository = eventRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Event[]>> Get(bool includeGigs = false)
+        {
+            try
+            {
+                var result = await _eventRepository.GetEvents(includeGigs);
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+
+
+    }
+}
--- ComedyEvents/Startup.cs
+++ ComedyEvents/Startup.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ComedyEvents.Context;
+using ComedyEvents.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
 
 namespace ComedyEvents
 {
@@ -18,7 +20,12 @@ namespace ComedyEvents
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<EventContext>();
-            services.AddMvc();
+            services.AddScoped<IEventRepository, EventRepository>();
+            services.AddMvc(options => options.EnableEndpointRouting = false)
+                .AddNewtonsoftJson(options =>
+                {
+                    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,7 +36,7 @@ namespace ComedyEvents
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
+            app.UseMvc();
 
             //app.UseEndpoints(endpoints =>
             //{
